81c41fa4a5071bac0b97fb125d2706465e73034b
Saumya  Brahmbhatt <139834472+Saumya812@users.noreply.github.com>
2025-07-11 10:50:36 -0400

    Create ocr_utils.py
    Phase 1 complete

---
+++ ocr_utils.py
@@ -0,0 +1,20 @@
+import pytesseract
+from PIL import Image
+import io
+import fitz  # PyMuPDF
+
+# Set Tesseract path
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+def extract_text_from_image(uploaded_file):
+    image = Image.open(io.BytesIO(uploaded_file.read()))
+    text = pytesseract.image_to_string(image)
+    return text
+
+def extract_text_from_pdf(uploaded_file):
+    pdf_text = ""
+    doc = fitz.open(stream=uploaded_file.read(), filetype="pdf")
+    for page in doc:
+        pdf_text += page.get_text()
+    return pdf_text
+
